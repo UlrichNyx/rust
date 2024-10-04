e8177b1a9838d75d1800cb10e12625c673a77c66
UlrichNyx <kontogiannisfilippos99@gmail.com>
2024-10-05 00:34:00 +0100

    Adding celsius to farenheit temperature converter

---
+++ temp_converter/src/main.rs
@@ -0,0 +1,58 @@
+use fltk::{app, button::Button, frame::Frame, input::Input, prelude::*, window::Window, button::CheckButton};
+
+fn main() {
+    let app = app::App::default();
+    let mut wind = Window::new(100, 100, 400, 300, "Temperature Converter");
+    let mut title = Frame::new(0, 0, 400, 50, "Convert from: ");
+    let mut frame = Frame::new(0, 170, 400, 50, "");
+    let input = Input::new(100, 80, 200, 30, "");
+    let mut but = Button::new(160, 210, 80, 40, "Convert");
+
+    // Create a CheckButton (toggle switch behavior)
+    let mut switch = CheckButton::new(160, 100, 80, 40, "use Farenheit");
+
+    // Set an initial state
+    switch.set_value(false);
+
+    // Set a callback to handle toggle behavior
+    switch.set_callback(move |sw| {
+        let mut mode = String::from("Farenheit");
+        if sw.is_checked() {
+            println!("Converting from Farenheit");
+            mode = "Farenheit".to_string();
+            
+        } else {
+            println!("Converting from Celsius");
+            mode = "Celsius".to_string();
+        }
+        let title_text = format!("Convert from {}", mode);
+        title.set_label(&title_text);
+    });
+    wind.end();
+    wind.show();
+
+    but.set_callback(move |_| {
+        let text = input.value(); // Get the input field's value
+        let result_num: Result<i32, std::num::ParseIntError> = text.parse();
+
+        match result_num {
+            Ok(num) => {
+                if switch.is_checked()
+                {
+                     let celsius = (num as f64 - 32.0) * (5.0/9.0);
+                     let text: String = celsius.to_string() + "°C";
+                     frame.set_label(&text);  
+                }
+                else {
+                    let farenheit: f64 = num as f64 * (9.0/5.0) + 32.0;
+                    let text: String = farenheit.to_string() + "°F";
+                    frame.set_label(&text);  
+                }
+
+            },
+            Err(e) => println!("Failed to parse number: {}", e),
+        }
+    });
+
+    app.run().unwrap();
+}
