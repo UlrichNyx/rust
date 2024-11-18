dc8338b32c91be26a2df18ceead967bc9468457a
UlrichNyx <kontogiannisfilippos99@gmail.com>
2024-11-18 22:33:15 +0000

    Continuing chapter 12 challenge

--- chapter_12/minigrep/src/main.rs
+++ chapter_12/minigrep/src/main.rs
@@ -1,30 +1,6 @@
 use std::env;
-use std::fs;
 use std::process;
-
-struct Config
-{
-    query: String,
-    filename: String
-}
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments!");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        Ok(Config {query: query, filename: filename})
-    }
-}
-
-fn run(config: Config) {
-    println!("Searching for {}", config.query);
-    println!("In file: {}", config.filename);
-
-    let contents = fs::read_to_string(config.filename).expect("Should have been able to read the file.");
-    println!("With text: \n {contents}"); 
-}
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -32,5 +8,9 @@ fn main() {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-    run(config);
+    if let Err(e) = minigrep::run(config)
+    {
+        println!("Application error {}", e);
+        process::exit(1);
+    }
 }
